0c4e1305bc1826c2663b080088c10866a4daf418
User1 <kadriidris41@yahoo.com>
2023-09-15 09:32:03 +0100

    commit pt.3 (Team2)

---
+++ src/main/java/Bank.java
@@ -0,0 +1,39 @@
+public class Bank {
+
+    int accountNumber;
+    String accountNameHolder;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountNameHolder() {
+        return accountNameHolder;
+    }
+
+
+
+    HashMap<Integer, Account> accountMap = new HashMap<Integer, Account>();
+
+    public void createAccount(double balance) {
+        if (!accountMap.containsKey(accountNumber)) {
+            Account account = new Account(balance);
+            accountMap.put(accountNumber, Account);
+            System.out.println("Account created");
+        }
+        else {
+            System.out.println("No!");
+        }
+
+
+    }
+
+    public Account getAccountByNumber() {
+
+        return null;
+    }
+
+    public void transferMoney() {
+
+    }
+}
